ba31631d9ecbf694e47afabf31595a6d9e1a9095
wrong89 <fenomen_03@mail.ru>
2024-11-17 01:14:19 +0300

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import Counter from '@/components/Counter/Counter';
+import './App.scss';
+
+function App() {
+    return (
+        <>
+            <Counter />
+        </>
+    );
+}
+
+export default App;
---
+++ client/vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+        },
+    },
+    plugins: [react()],
+});
