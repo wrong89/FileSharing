479a5a8ca745f82b9ddd08946fca23d7ef9715ef
wrong89 <fenomen_03@mail.ru>
2024-11-26 00:21:27 +0300

    added svgr

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,5 +16,5 @@ export default defineConfig({
             '@config': path.resolve(__dirname, './src/config/'),
         },
     },
-    plugins: [react()],
+    plugins: [react(), svgr()],
 });
